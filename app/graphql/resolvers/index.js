e232e8ac4d7f0561d4ef5b2e7f832606cd13ff1e
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 14:53:13 -0500

    Fix problems with existing queries, add new queries/mutations for forum categories

--- app/graphql/resolvers/index.js
+++ app/graphql/resolvers/index.js
@@ -6,7 +6,9 @@ const resolvers = {
      * Returns an array of users
      */
     users: async () => {
-      const users = await knex.from('users').select('id', 'username');
+      const users = await knex
+        .from('users')
+        .select('id', 'username', 'email', 'is_staff');
       return users;
     },
     /**
@@ -14,10 +16,53 @@ const resolvers = {
      */
     user: async (_, args) => {
       const user = await knex('users')
-        .select('id', 'username')
+        .select('id', 'username', 'email', 'is_staff')
         .where({ username: args.username })
         .first();
       return user;
+    },
+
+    forumCategories: async () => {
+      const forumCategories = await knex
+        .from('forum_categories')
+        .select('id', 'name');
+      return forumCategories;
+    },
+
+    forumCategory: async (_, args) => {
+      const forumCategory = await knex('forum_categories')
+        .select('id', 'name')
+        .where({ name: args.name })
+        .first();
+      return forumCategory;
+    },
+  },
+  Mutation: {
+    createUser: async (obj, { input }) => {
+      const user = await knex('users').insert({
+        username: input.username,
+        email: input.email,
+      });
+
+      const createdUser = await knex('users')
+        .select('id', 'username', 'email', 'is_staff')
+        .where({ username: input.username })
+        .first();
+
+      return createdUser;
+    },
+
+    createForumCategory: async (obj, { input }) => {
+      const forumCategory = await knex('forum_categories').insert({
+        name: input.name,
+      });
+
+      const createdForumCategory = await knex('forum_categories')
+        .select('id', 'name')
+        .where({ name: input.name })
+        .first();
+
+      return createdForumCategory;
     },
   },
 };
