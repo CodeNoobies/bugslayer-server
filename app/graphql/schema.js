cf6d32c4085f7f6bc217832fe959a8c7e8cf05f4
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 14:51:54 -0500

    Add comments and new mutations/queries for the forum categories

--- app/graphql/schema.js
+++ app/graphql/schema.js
@@ -1,8 +1,29 @@
-const Query = `
+const { gql } = require('apollo-server');
+
+const Query = gql`
   type Query {
+    "Get a list of all the users registered"
     users: [User!]!
+    "Get the data for an individual user based on its username"
     user(username: String!): User
+
+    "Get a list of all the forum categories"
+    forumCategories: [ForumCategory!]!
+    "Get the data for an individual forum category based on its name"
+    forumCategory(name: String!): ForumCategory
   }
 `;
 
-module.exports = Query;
+const Mutation = gql`
+  type Mutation {
+    "Create a new user"
+    createUser(input: CreateUserInput): User
+    "Create a new forum category"
+    createForumCategory(input: CreateForumCategoryInput): ForumCategory
+  }
+`;
+
+module.exports = {
+  Query,
+  Mutation,
+};
