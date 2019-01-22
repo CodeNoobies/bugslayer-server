fad53cb5e716556256cd0f1b702a410a77d2692d
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 14:52:47 -0500

    Comment user type

--- app/graphql/typedefs/user.js
+++ app/graphql/typedefs/user.js
@@ -1,7 +1,24 @@
-const typeDef = `
+const { gql } = require('apollo-server');
+
+const typeDef = gql`
+  "An user of the application"
   type User {
     id: Int!
     username: String!
+    email: String!
+    is_staff: Boolean!
+  }
+
+  "Input required to create a new user"
+  input CreateUserInput {
+    username: String!
+    email: String!
+  }
+
+  "Input required to update an existing user"
+  input UpdateUserInput {
+    username: String!
+    email: String!
   }
 `;
 
