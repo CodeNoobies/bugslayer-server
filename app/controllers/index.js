2381d3efccd3243c6b91aa13dabb5ab5af618df7
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 22:56:22 -0500

    Export the newly created functions

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,18 +2,24 @@ const {
   getAllUsers,
   getUserByID,
   getUserByUsername,
+  createUser,
 } = require('./users_controller');
 const {
   getAllForumCategories,
   getForumCategoryByID,
   getForumCategoryByName,
+  createForumCategory,
 } = require('./forum_categories_controller');
+const { getAllForumThreads } = require('./forum_threads_controller');
 
 module.exports = {
   getAllUsers,
   getUserByID,
   getUserByUsername,
+  createUser,
   getAllForumCategories,
   getForumCategoryByID,
   getForumCategoryByName,
+  createForumCategory,
+  getAllForumThreads,
 };
