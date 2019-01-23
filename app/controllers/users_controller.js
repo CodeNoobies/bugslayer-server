01c187a2fbd8228c76ee3f43530b0e246f8f3a0f
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 23:06:36 -0500

    Document functions on users controller

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -1,12 +1,24 @@
 const knex = require('../db');
 
+/**
+ * List of fields we want to query every time
+ */
 const LIST_OF_FIELDS = ['id', 'username', 'email', 'is_staff'];
 
+/**
+ * Returns a list of all the users in the database
+ * @return {Array} Array of objects
+ */
 const getAllUsers = async () => {
   const users = knex.from('users').select(...LIST_OF_FIELDS);
   return users;
 };
 
+/**
+ * Returns an user given an id
+ * @param {int} id - ID used to find the user in the database
+ * @return {object} User object
+ */
 const getUserByID = async id => {
   const user = knex('users')
     .select(...LIST_OF_FIELDS)
@@ -15,6 +27,11 @@ const getUserByID = async id => {
   return user;
 };
 
+/**
+ * Returns an user given an username
+ * @param {string} username - Username used to find the user in the database
+ * @return {object} User object
+ */
 const getUserByUsername = async username => {
   const user = knex('users')
     .select(...LIST_OF_FIELDS)
@@ -23,7 +40,11 @@ const getUserByUsername = async username => {
   return user;
 };
 
-const createUser = async user => {
+/**
+ * Saves a new user into the database
+ * @param {object} user - User object containing an username and an email
+ */
+const saveUser = async user => {
   await knex('users').insert({
     username: user.username,
     email: user.email,
@@ -34,5 +55,5 @@ module.exports = {
   getAllUsers,
   getUserByID,
   getUserByUsername,
-  createUser,
+  saveUser,
 };
