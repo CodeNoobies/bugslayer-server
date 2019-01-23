1820731677d01745dbc5ee50302f585a530df775
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 22:25:42 -0500

    Add forum thread to the list of exported type definitions

--- app/graphql/typedefs/index.js
+++ app/graphql/typedefs/index.js
@@ -1,7 +1,9 @@
 const User = require('./user');
 const ForumCategory = require('./forum_category');
+const ForumThread = require('./forum_thread');
 
 module.exports = {
   User,
   ForumCategory,
+  ForumThread,
 };
