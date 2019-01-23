3105809d27d83989bb8e1eb1cc0230a12ed9c61a
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 22:24:48 -0500

    Create controller for users

---
+++ app/controllers/users_controller.js
@@ -0,0 +1,30 @@
+const knex = require('../db');
+
+const LIST_OF_FIELDS = ['id', 'username', 'email', 'is_staff'];
+
+const getAllUsers = async () => {
+  const users = knex.from('users').select(...LIST_OF_FIELDS);
+  return users;
+};
+
+const getUserByID = async id => {
+  const user = knex('users')
+    .select(...LIST_OF_FIELDS)
+    .where({ id })
+    .first();
+  return user;
+};
+
+const getUserByUsername = async username => {
+  const user = knex('users')
+    .select(...LIST_OF_FIELDS)
+    .where({ username })
+    .first();
+  return user;
+};
+
+module.exports = {
+  getAllUsers,
+  getUserByID,
+  getUserByUsername,
+};
