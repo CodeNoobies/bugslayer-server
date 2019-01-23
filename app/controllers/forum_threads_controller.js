263a9ad6ce9dd8286f55ceb34376e7ff4f41dc36
Oxyrus <Oxyrus@users.noreply.github.com>
2019-01-22 23:19:52 -0500

    Document forum threads controller

--- app/controllers/forum_threads_controller.js
+++ app/controllers/forum_threads_controller.js
@@ -1,9 +1,16 @@
 const knex = require('../db');
 
+/**
+ * List of fields we want to query every time
+ */
 const LIST_OF_FIELDS = ['id', 'title', 'user_id', 'forum_category_id'];
 
+/**
+ * Returns a list of all the forum threads in the database
+ * @return {Array} Array of objects
+ */
 const getAllForumThreads = async () => {
-  const forumThreads = knex.select(...LIST_OF_FIELDS).from('forum_threads');
+  const forumThreads = knex('forum_threads').select(...LIST_OF_FIELDS);
   return forumThreads;
 };
 
